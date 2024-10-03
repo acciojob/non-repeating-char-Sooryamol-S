a5c8823a9a6ae155f1fe0ebb24a83976434dd863
Sooryamol-S <135025674+Sooryamol-S@users.noreply.github.com>
2024-10-03 11:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+for(let i=0;i<str.length;i++){
+	let s=str.charAt(i);
+	let p=str.charAt(i+1);
+	if(i==0){
+		if(s!=p){
+			return s;
+		}
+	}
+	if(i>0 && i<str.length-1){
+		let m=str.charAt(i-1);
+		if(s!=p && s!=m){
+          return s;
+		}
+	}
+	if(i==str.length-1){
+		if(s!=m){
+         return s;
+		}
+	}
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+	return null;
+}
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
